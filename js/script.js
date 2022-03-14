40c93a4276f7e149b7c4e36fdbec5fccccef20a7
Małgorzata Kopydłowska <niezapominajka@kodilla.com>
2022-03-14 15:53:54 +0100

    Add new changes

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@
 function titleClickHandler(event) {
     event.preventDefault();
     const clickedElement = this;
-    console.log('Link was clicked', event);
+    console.log('Link was clicked !', event);
     const activeLinks = document.querySelectorAll('.titles a.active');
     for(let activeLink of activeLinks) {
         activeLink.classList.remove('active');
@@ -28,28 +28,14 @@ function titleClickHandler(event) {
     console.log('targetArticle', targetArticle);
     }
 
-    const titleClickHandler = function(event){
-        console.log('Link was clicked!');
-
-    /* remove class 'active' from all article links  */
-
-    /* add class 'active' to the clicked link */
-
-    /* remove class 'active' from all articles */
-
-    /* get 'href' attribute from the clicked link */
-
-    /* find the correct article using the selector (value of 'href' attribute) */
-
-    /* add class 'active' to the correct article */
-
-    }
-
+  /*
   const links = document.querySelectorAll('.titles a');
-
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
-    }
+  }
+  */
+
+   
 
 
 
